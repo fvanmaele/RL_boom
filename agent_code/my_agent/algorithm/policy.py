3363db2abba057e5921965fec4310a162b45ad1a
Lily D <lily@stud.uni-heidelberg.de>
2019-03-04 10:03:06 +0100

    added my agent and also policy class

---
+++ agent_code/my_agent/algorithm/policy.py
@@ -0,0 +1,12 @@
+﻿class Policy:
+
+    def __init__(self, state, action, reward):
+        valid_actions = []
+        self.state = state
+        if action in valid_actions:
+            self.action = action
+        else:
+            print("ERROR: INVALID ACTION")
+        self.reward = reward
+    
+    
