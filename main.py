b6b7d1fdfcd246b304815b8cb032807afdc90471
Daniel Gonzalez <daniel.gonzalez.alv@gmail.com>
2019-03-09 21:41:55 +0100

    no important changes
    hier wurde nichts
    wichtiges gemacht

--- main.py
+++ main.py
@@ -41,10 +41,10 @@ def main():
 
     # Initialize environment and agents
     world = BombeRLeWorld([
+            ('random_agent', False),
+            ('random_agent', False),
             ('simple_agent', False),
-            ('simple_agent', False),
-            ('simple_agent', False),
-            ('simple_agent', False)
+            ('my_agent', True)
         ])
     # world = ReplayWorld('Replay 2019-01-30 16:57:42')
     user_inputs = []
