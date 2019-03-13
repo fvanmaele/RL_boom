7b947feb70a1ffcac2a0913b03adc4fda74a3038
Lily D <lily@stud.uni-heidelberg.de>
2019-03-13 17:37:35 +0100

    added numpy implementation detail

--- agent_code/my_agent/algorithm/QLearningLinApprox/LinearApproxQLearning.py
+++ agent_code/my_agent/algorithm/QLearningLinApprox/LinearApproxQLearning.py
@@ -4,8 +4,6 @@
 Q Learning using linear approximation, with feature selection f as a function
 for modularity 
 
-!!!Implementation details to follow (numpy functions for vector multiplications)!!!
-
 all possible actions are saved here for simplicity as a global parameter.
 '''
 
@@ -31,9 +29,9 @@ def update_w(state, action, next_state, reward, w, f, step_size=0.05, discount=1
 def update_w_greedy(state, action, q, reward, w, f, step_size=0.05, discount=1):
     vec_state = f(state, action)
         
-    delta = reward + discount*q - (w.T*f(state,action))
+    delta = reward + discount*q - np.dot(w.T*f(state,action))
     
-    return w+step_size * delta * f(state,action)
+    return w + step_size * delta * f(state,action)
    
 
 def pred(game_state, w, f):
@@ -49,7 +47,7 @@ def pred(game_state, w, f):
     best_a = 'WAIT'
     
     for a in actions:
-        q = w.T*f(game_state, a)
+        q = np.dot(w.T*f(game_state, a))
         if max_q<q:
             best_q = q
             best_a = a
