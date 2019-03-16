2454af353b25254796f3c280ed1383cf88d916ec
Ferdinand Vanmaele <fvanmaele@ciaconna.org>
2019-03-16 20:02:40 +0100

    feature_extraction: check for bombs+agents as obstacles

--- agent_code/my_agent/algorithm/feature_extraction.py
+++ agent_code/my_agent/algorithm/feature_extraction.py
@@ -1,18 +1,75 @@
+import settings as game_settings
+
 # Save all actions in the action space in a global variable for
 # simplicity.
 actions = ['UP', 'DOWN', 'LEFT', 'RIGHT', 'BOMB', 'WAIT']
 
-def bulk_feature(feat, state):
+# Function adapted from items.py.
+def get_blast_coords(x, y, arena):
+    """Retrieve the blast range for a bomb.
+
+    The maximal power of the bomb (maximum range in each direction) is
+    imported directly from the game settings. The blast range is
+    adjusted according to walls (immutable obstacles) in the game
+    arena.
+
+    Parameters:
+    * x, y:  Coordinates of the bomb.
+    * arena: 2-dimensional array describing the game arena.
+
+    Return Value:
+    * Array containing each coordinate of the bomb's blast range.
+    """
+    blast_coords = [(x,y)]
+    power = game_settings.settings['bomb_power']
+
+    for i in range(1, power+1):
+        if arena[x+i, y] == -1:
+            break
+        blast_coords.append((x+i, y))
+    for i in range(1, power+1):
+        if arena[x-i, y] == -1:
+            break
+        blast_coords.append((x-i, y))
+    for i in range(1, power+1):
+        if arena[x, y+i] == -1:
+            break
+        blast_coords.append((x, y+i))
+    for i in range(1, power+1):
+        if arena[x, y-i] == -1:
+            break
+        blast_coords.append((x, y-i))
+
+    return blast_coords
+
+
+# Function adapted from environment.py
+def tile_is_free(x, y, arena, bombs, others):
+    is_free = (arena[x,y] == 0)
+
+    if is_free:
+        for obstacle in bombs + others:
+            is_free = is_free and (obstacle.x != x or obstacle.y != y)
+    return is_free
+
+
+def vectorized_feature(feature, state):
+    """Return a vector containg a feature for each available action.
+
+    Parameters:
+    * feature: Callable representing a feature vector F(s, a).
+    * state:   Array representing the state s.
+    """
     results = []
 
     for a in actions:
-        results.append(feat(state, action))
+        results.append(feature(state, action))
     return results
 
 
 def feature_1(state, action):
     """Feature 1
-    
+
     Value 1: Action is the first step in reaching a coin in the arena.
     Value 0: Otherwise.
 
@@ -28,133 +85,84 @@ def feature_2(state, action):
     Value 1: Action would most likely take the agent from
              state s into a location where the agent could die.
     Value 0: Otherwise.
-
-    The only way for an agent to die is by crossing an exploding
-    bomb.  The rules are the following:
-
-    "Once a bomb is dropped, it will detonate after four steps and
-    crate an explosion that extends three tiles up, down, left and
-    right.  The explosion destroys crates and agents, but will
-    stop at stone walls and does not reach around corners. Agents
-    can only drop a new bomb after their previous one has
-    exploded."
-
-    Performing an "evasive maneuver" (that is, taking the agent
-    from a threatened state s into a location where the agent is
-    no longer likely to die) is a special case of assigning 0.
     """
-    # Retrieve arena, agent and bomb information
     arena = state['arena']
     bombs = state['bombs']
+    others = state['others']
+    explosions = state['explosions']
     x, y, _, _ = state['self']
 
-    # All placed bombs (by several agents, ourselves included) are
-    # considered in succession.
-    for xb, yb, timer in bombs:
-        if timer < 0:
-            # Bomb will explode on the next action, unless the agent
-            # performs an evasive maneuver, or is not in the blast
-            # range of the bomb.
-            blast_range_h = []
-            blast_range_v = []
+    # TODO: We can replace this stuff later with a directions array,
+    # and have feature_2 return a vector for each action.
+    if action == 'UP':
+        x_next, y_next = x, y-1
+    elif action == 'DOWN':
+        x_next, y_next = x, y+1
+    elif action == 'LEFT':
+        x_next, y_next = x-1, y
+    elif action == 'RIGHT':
+        x_next, y_next = x+1, y
+    elif action == 'BOMB' or action == 'WAIT':
+        x_next, y_next = x, y
 
-            # Check three tiles up, left, right and down of bomb
-            # for a wall. Crates and other agents are destroyed by
-            # the bomb and do not affect the blast range.
-            # 
-            # TODO: get_bomb_coords in item.py (bomb class) is
-            # very similar, apart from using a single list
-            # instead of two.
-            for i in range(1, 5): # {1,..,4}
-                if arena[xb-i, yb] == -1: # left
-                    blast_range_h.append((xb-i+1, yb))
-                    break
-            for i in range(1, 5):
-                if arena[xb+i, yb] == -1: # right
-                    blast_range_h.append((xb+i-1, yb))
-                    break
-            for i in range(1, 5):
-                if arena[xb, yb+i] == -1: # down (reversed)
-                    blast_range_v.append((xb, yb+i-1))
-                    break
-            for i in range(1, 5):
-                if arena[xb, yb-i] == -1: # up (reversed)
-                    blast_range_v.append((xb, yb-i+1))
-                    break
+    # Check if the tile reached by the next action is free. Note tha 
+    # that may be destroyed by bombs, but prevents us from moving into
+    # a free tile.
+    if tile_is_free(x_next, y_next, arena, bombs, active_agents):
+        # Set the next action as equivalent to waiting. (Performing
+        # this action will result in an invalid action, and is handled
+        # by a different feature.)
+        x_next, y_next = x, y
+    
+    # Check if the agent will move into the blast range of an existing
+    # bomb. This information is only available when the bomb has
+    # already exploded, and has to be calculated manually when not.
+    if explosions[(x_next, y_next)] > 0:
+        return 1
 
-            # TEST: A bomb cannot be fully enclosed by walls and
-            # must have a blast range.
-            if len(blast_range_h) == len(blast_range_v) == 1:
-                raise ValueError("bomb may not be enclosed by walls")
-            if len(blast_range_h) == 0 or len(blast_range_v) == 0:
-                raise ValueError("bomb without blast range")
+    # Check if the agent will move into the blast range of a bomb that
+    # explodes on the next time step. As the 'explosions' array does
+    # not yet contain the blast range, we compute it manually by
+    # iterating over each bomb.
+    for xb, yb, t in bombs:
+        if timer < 0 and (x_next, y_next) in get_blast_coords(xb, yb, arena):
+            return 1
 
-            # TEST: Check if range of blast radius is correct.
-            if not (1 <= blast_range_h[-1] - blast_range_h[0] <= 7 and
-                    1 <= blast_range_v[-1] - blast_range_v[0] <= 7):
-                raise ValueError("bomb has invalid blast range")
-
-            # If the agent is in the blast range, check if the
-            # action allows to perform an evasive maneuver. While
-            # crates do not provide cover from the blast, they
-            # block a free tile where the agent may otherwise move
-            # for cover.
-            if (blast_range_h[0] <= x <= blast_range_h[-1] and
-                blast_range_v[0] <= y <= blast_range_v[-1]):
-                if (action == 'WAIT' or action == 'BOMB' or action == 'INVALID_ACTION'):
-                    return 1 # Agent threat found
-                elif (action == 'UP' and not arena[x, y-1] == 1):
-                    if blast_range_v[0] <= (y)-1 <= blast_range_v[-1]:
-                        return 1
-                elif (action == 'DOWN' and not arena[x, y+1] == 1):
-                    if blast_range_v[0] <= (y)+1 <= blast_range_v[-1]:
-                        return 1
-                elif (action == 'LEFT' and not arena[x-1, y] == 1):
-                    if blast_range_h[0] <= (x)-1 <= blast_range_h[-1]:
-                        return 1
-                elif (action == 'RIGHT' and not arena[x+1, y] == 1):
-                    if blast_range_h[0] <= (x)+1 <= blast_range_h[-1]:
-                        return 1
-                else:
-                    raise AttributeError("invalid action for bomb maneuver")
-        else:                
-            continue # Bomb will explode in some later time step.
-
-        # No threats to the agent were found.
-        return 0
+    # If the above conditions are not satisfied, the agent is most
+    # likely safe from death by taking the given action.
+    return 0
 
 
 def feature_3(state, action):
     """Feature 3
 
     Value 1: Action most likely takes the agent from state s into a
-             location where there is an obstacle (wall or crate).
+             location where there is an obstacle (wall, crate, bomb or
+             other agent in the arena).
     Value 0: Otherwise.
     """
     # Retrieve agent position.
     arena = state['arena']
+    bombs = state['bombs']
+    others = state['others']
     x, y, _, _ = state['self']
 
-    # Taking the agent into a wall is one possible invalid action.
     if action == 'INVALID_ACTION':
+        # Taking the agent into a wall is one possible invalid action.
         return 1
-    elif action == 'WAIT' or action == 'BOMB':
-        # Placing a bomb or waiting does not result in movement.
-        return 0
     elif action == 'UP':
-        if arena[x, y-1] in (-1, 1):
+        if not tile_is_free(x, y-1, arena, bombs, others):
             return 1
     elif action == 'DOWN':
-        if arena[x, y+1] in (-1, 1):
+        if not tile_is_free(x, y+1, arena, bombs, others):
             return 1
     elif action == 'LEFT':
-        if arena[x-1, y] in (-1, 1):
+        if not tile_is_free(x-1, y, arena, bombs, others):
             return 1
     elif action == 'RIGHT':
-        if arena[x+1, y] in (-1, 1):
+        if not tile_is_free(x+1, y, arena, bombs, others):
             return 1
-
-    # No obstacle found.
+    # No obstacle found, or the next action is not a movement action.
     return 0
 
 
@@ -164,6 +172,7 @@ def feature_4(state, action):
     Reward the minimal distance to move the agent away from a zone (of
     certain radius) containing a bomb.
     """
+    pass
 
 
 def feature_5(state, action):
