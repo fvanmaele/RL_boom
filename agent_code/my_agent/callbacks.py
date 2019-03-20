7b493577c9426cff9c6efad2be2f95678d2b0b15
Ferdinand Vanmaele <fvanmaele@ciaconna.org>
2019-03-20 14:37:42 +0100

    add self.actions, remove unused features

--- agent_code/my_agent/callbacks.py
+++ agent_code/my_agent/callbacks.py
@@ -7,47 +7,10 @@ from agent_code.my_agent.algorithms import *
 #from agent_code.my_agent.features import *
 #from agent_code.my_agent.functions import *
 
-def tmp_feature4(game_state):
-    '''
-    This feature rewards the action that minimizes the distance to safety
-    should the agent be in the danger zone(where explosions will be).
-        F(s,a) = 1, should a reduces distance to safety
-        F(s,a) = 0, otherwise
-    F(s,a) returns 0 if we are not in the danger zone   
-    
-    We begin by extracting all relevant information from game_state
-    '''
-    x, y, _, bombs_left = game_state['self']
-    arena = game_state['arena']
-    bombs = game_state['bombs']
-    bombs_xy = [(x,y) for (x,y,t) in bombs]
-    others = [(x,y) for (x,y,n,b) in game_state['others']]
-    directions = [(x,y-1), (x,y+1), (x-1,y), (x+1,y)]
-
-    '''
-    and initializing the resulting vector
-    '''
-    features = []
-
-    danger_zone = [] 
-    if len(bombs) != 0:
-        for b in bombs_xy:
-            danger_zone += compute_blast_coords(arena, b)
-    else:
-        return np.array([0,0,0,0,0,0])
-
-    danger_zone += bombs_xy
-
-    for d in directions:
-        if ((arena[d] != 0) or (d in others) or (d in bombs_xy)):
-            d = (x,y)
-        if d in danger_zone:
-            j
-        arena[danger_zone] = 5       
-        target = np.argwhere(arena==0)
-    #d = look_for_targets(free_space, (agent[0], agent[1]), safe_loc)
 def setup(self):
-     
+    # Assumed order for features (algorithms.py)
+    self.actions = ['UP', 'DOWN', 'LEFT', 'RIGHT', 'BOMB', 'WAIT']
+
     # load weights
     try:
         self.weights = np.load('./agent_code/my_agent/models/weights.npy')
@@ -64,12 +27,8 @@ def setup(self):
     self.gamma = 0.9
     self.EPSILON = 0.2
 
+
 def act(self):
-
-    """
-    actions order: 'UP', 'DOWN', LEFT', 'RIGHT', 'BOMB', 'WAIT'    
-    """
-
     # load state 
     game_state = self.game_state  # isn't it memory waste calling in each feature extraction for coins, self, arena?
     print("\n", game_state['step'])
@@ -101,20 +60,17 @@ def act(self):
     print("f6 ", f6)
     f7 = feature7(game_state) # reward action
     print("f7 ", f7)
-    f8 = feature8(game_state) # rewards good action
-    print("f8 ", f8)
+    # f8 = feature8(game_state)
+    # print("f8 ", f8)
     f9 = feature9(game_state) # rewards good action
     print("f9 ", f9)
     f10 = feature10(game_state) # rewards good action
     print("f10 ", f10)
-    feature_state = np.vstack((f0,f1,f2,f3,f4,f5,f6,f7,f8,f9,f10)).T
+    feature_state = np.vstack((f0,f1,f2,f3,f4,f5,f6,f7,f9,f10)).T
 
-    #feature9(game_state) # rewards good action
-    #print("f9 ", f9)
 
     self.prev_state = feature_state
-    weights = np.array([1,1,-7,-0.5,4,-0.5,1.5,2,0.5,0.5,0.5])   #initial guess 
-    #weights = np.array([1,1,-5,4,-0.5,1,1])   #initial guess 
+    weights = np.array([1,1,-7,-1,4,-0.5,1.5,2,0.5,0.5])   #initial guess 
     print("weights", weights)
 #    # later no necessary
 #    if self.weights == []:
@@ -130,28 +86,20 @@ def act(self):
     print("q", q_approx)
     best_actions = np.where(q_approx == np.max(q_approx))[0] 
     shuffle(best_actions)
-    q_next_action = s.actions[best_actions[0]] #GREEDY POLICY
-#
-#    # Pick action
-#    self.logger.info('Pick action')
+    q_next_action = self.actions[best_actions[0]] #GREEDY POLICY
 
-#    self.next_action = np.random.choice(['WAIT','RIGHT', 'LEFT', 'DOWN', 'UP','BOMB'], p=[0.15, .15, 0.15, .15, 0.15, 0.25])
-
-#    print("action picked  ", q_next_action)
     self.next_action = q_next_action
     print(self.next_action)
 
-def reward_update(self):
 
-    '''
-        IMPORTANT: 
-            reward_update happens AFTER act
-            This means, self.next_action in reward_update is the
-            action the algorithm just took in action. This is why
-            we should rename self.next_action as prev_action in 
-            reward_update
-        
-    '''
+def reward_update(self):
+    """IMPORTANT:
+
+    reward_update happens AFTER act This means, self.next_action in
+    reward_update is the action the algorithm just took in
+    action. This is why we should rename self.next_action as
+    prev_action in reward_update
+    """
     self.logger.info('IN TRAINING MODE ')
 
     reward = 0 
@@ -175,7 +123,6 @@ def reward_update(self):
         # learning 
     
         # Berechene alle features
-        next_state = feat_1(self.game_state)
         """
         so wie vorhin und mit stack, vlt, machen wir eine Function die alles auf einmal stack
         """
