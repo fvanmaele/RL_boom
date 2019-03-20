be401a58d4c4d93c3d59eff8fae3e9b2e231c04d
Lily D <lily@stud.uni-heidelberg.de>
2019-03-20 19:22:51 +0100

    training setup

--- agent_code/my_agent/callbacks.py
+++ agent_code/my_agent/callbacks.py
@@ -11,7 +11,7 @@ def setup(self):
      
     # load weights
     try:
-        self.weights = np.load('./agent_code/my_agent/models/weights_initT.npy')
+        self.weights = np.load('./agent_code/my_agent/models/weights_initX.npy')
         print("weights loaded")
     except:
         self.weights = []
@@ -22,22 +22,21 @@ def setup(self):
     
     # Step size or gradient descent 
     self.alpha = 0.2 
-    self.gamma = 1
+    self.gamma = 0.95
     self.EPSILON = 0.25
     self.round = 1
     self.training_weights = []
     
-    print("############# {} round: {} ###############".format('init with 1s', self.round))
-#    # While this timer is positive, agent will not hunt/attack opponents
-#    self.ignore_others_timer = 0
-#########################################################################
+    print("############# {} round: {} ###############".format('', self.round))
+
+#####################################################################
 
 def act(self):
-
+    
     """
     actions order: 'UP', 'DOWN', LEFT', 'RIGHT', 'BOMB', 'WAIT'    
     """
-
+    
     # load state 
     game_state = self.game_state  # isn't it memory waste calling in each feature extraction for coins, self, arena?
     
@@ -56,7 +55,7 @@ def act(self):
     f1 = feature1(game_state) # reward good action
     f2 = feature2(game_state) # penalization bad action
     f3 = feature3(game_state)
-    f4 =feature4(game_state) # reward good action
+    f4 = feature4(game_state) # reward good action
     f5 = feature5(game_state)  # penalize bad action
     f6 = feature6(game_state)  # reward good action
     f7 = feature7(game_state) # reward action
@@ -75,20 +74,32 @@ def act(self):
     
     #later no necessary
     if len(self.weights) == 0:
-        #print('no weights, init weights')
+        print('no weights, init weights')
         self.weights = np.array([1,1,-7,-1,4,-0.5,1.5,2,0.5,0.5,-7,1.5,3,2,-1])   #initial 
         #self.weights = np.ones(feature_state.shape[1])  
-        self.weights = np.random.rand(15)
-        self.training_weights.append(self.weights)
+        #self.weights = np.random.rand(feature_state.shape[1])
+        #self.training_weights.append(self.weights)
         #self.weights = weights
     
     print(self.weights)
-    #print(self.game_state['step']) 
-    #print(feature_state)
+
 
     self.logger.info('Pick action')
     
-    # Linear approximation approach with epsilon-greedy
+    #'''
+    
+    # Linear approximation approach
+    q_approx = np.dot(feature_state, self.weights)    
+    best_actions = np.where(q_approx == np.max(q_approx))[0] 
+    shuffle(best_actions)
+
+    q_next_action = s.actions[best_actions[0]] #GREEDY POLICY
+    self.next_action = q_next_action
+    print("q action picked  ", q_next_action)
+    #'''
+    
+    ####### EPSILON GREEDY (TRAINING) #########################
+    '''
     greedy = np.random.choice([0,1], p=[self.EPSILON, 1-self.EPSILON])
     if greedy:
     
@@ -103,6 +114,10 @@ def act(self):
     else:
         self.next_action = np.random.choice(['RIGHT', 'LEFT', 'UP', 'DOWN', 'BOMB'], p=[.23, .23, .23, .23, .08])
         print("random action picked ", self.next_action)
+    '''
+    
+    
+    
     
 def reward_update(self):
 
@@ -124,20 +139,20 @@ def reward_update(self):
         if event == e.BOMB_DROPPED:
             reward += 1
         elif event == e.COIN_COLLECTED:
-            reward += 100
+            reward += 200
         elif event == e.KILLED_SELF:
             reward -= 100
         elif event == e.CRATE_DESTROYED:
             reward += 10
         elif event == e.COIN_FOUND:
-            reward += 30
+            reward += 100
         elif event == e.KILLED_OPPONENT:
             reward += 300
         elif event == e.GOT_KILLED:
             reward -= 300
         elif event == e.SURVIVED_ROUND:
             reward += 100
-        elif event == e.WAITED or e.INVALID_ACTION:
+        elif event == e.INVALID_ACTION:
             reward -= 2
 
         
@@ -172,25 +187,27 @@ def reward_update(self):
     f13 = feature13(self.game_state)
     f14 = feature14(self.game_state)
     f15 = feature15(self.game_state)
-    next_state = np.vstack((f0,f1,f2,f4,f5,f6,f7,f8,f9,f10)).T
-    
+    next_state = np.vstack((f0,f1,f2,f3,f4,f5,f6,f7,f9,f10,f11,f12,f13,f14,f15)).T
 
-    prev_state_a = self.prev_state[s.actions.index(self.next_action),:]
+    if self.game_state['step'] > 1:
 
-    # update weights
-    weights = q_gd_linapprox(next_state, prev_state_a, reward, self.weights, self.alpha, self.gamma)        
-    self.weights = weights
-    self.training_weights.append(self.weights)
-    # update alpha and gamma for convergence
-    self.alpha = 1/self.game_state['step']
-    self.gamma = self.gamma ** self.game_state['step']
-    # update epsilon ... TODO
+        prev_state_a = self.prev_state[s.actions.index(self.next_action),:]
+
+        # update weights
+        weights = q_gd_linapprox(next_state, prev_state_a, reward, self.weights, self.alpha, self.gamma)      
+        self.weights = weights        
+        self.training_weights.append(self.weights)
+        
+        # update alpha and gamma for convergence
+        self.alpha = 1/self.game_state['step']
+        #self.gamma = self.gamma ** self.game_state['step']
+        # update epsilon ... TODO
         
 
 def end_of_episode(self):
     self.round += 1
-    #np.save('./agent_code/my_agent/models/weights_init1.npy', self.weights)
-    file = open('initRand_epsGreedA0.2G0.8EPS0.25.pickle', 'wb')
+    np.save('./agent_code/my_agent/models/weights_initX.npy', self.weights)
+    file = open('initX_GreedA0.2G1.pickle', 'wb')
     pickle.dump(self.training_weights, file)
     
 def q_gd_linapprox(next_state, prev_state_a, reward, weights, alpha, gamma):
