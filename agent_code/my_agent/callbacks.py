bf78fe5314a11372ddce097b238f50f8e10339a2
Lily D <lily@stud.uni-heidelberg.de>
2019-03-22 15:47:50 +0100

    updated algorithms and callbacks

--- agent_code/my_agent/callbacks.py
+++ agent_code/my_agent/callbacks.py
@@ -1,94 +1,20 @@
 import numpy as np
-from random import shuffle
+import pickle
 from settings import e
-#from agent_code.my_agent.algorithms import *
-
-def compute_patch(arena, p1, p2):
-    """
-    this function computes the patch of the arena between the points p1 and p2
-    """
-    patch = arena[min(p1[1], p2[1]):max(p1[1], p2[1])+1,  min(p1[0], p2[0]):max(p1[0], p2[0])+1] 
-    return patch
-
-def feat_1(game_state):
-    """
-        Feature extraction for coin detection
-    """
-    coins = game_state['coins']
-    x, y, _, bombs_left = game_state['self']
-    directions = [(x,y), (x+1,y), (x-1,y), (x,y+1), (x,y-1)] # define directions
-    arena = game_state['arena']
-
-    feature = [] # Desired feature
-
-    for d in directions:
-        f = []  # array that helps building the features 
-        if arena[d] != 0:
-            d = directions[0]   # Don't move if it is an invalid action
-
-        diff2coins = np.abs( np.asarray(coins) - np.array([d[0],d[1]]) )
-        sum_diff2coins = np.sum(diff2coins, axis=1)
-
-        # min distance along x & y axis and 'global min distance'
-        diffmin_xy = np.min(diff2coins, axis=0)  # min difference of distances
-        min_coin, dist2min_coin = np.argmin(sum_diff2coins), np.min(sum_diff2coins)
-        
-        list_mincoins = list(np.where(sum_diff2coins == dist2min_coin)[0])
-        
-        # find how many walls are in between (finding the patches between the agent and coin)
-        patches = [] 
-        for m in list_mincoins:
-            p = compute_patch(arena, coins[m], d)
-            patches.append(p)
-
-        # not necesary any more because we are computing all the patches
-        patch_coinagent = compute_patch(arena, coins[min_coin], d)
-        
-        # look if there is a fast path to the closes coin
-        FAST_PATH = False 
-        for patch in patches:
-            if patch.shape[0] == 1 or patch.shape[1] == 1:
-                if np.count_nonzero(patch) == 0:
-                    FAST_PATH=True
-                    break
-            else:
-                FAST_PATH=True
-                break
-        if not FAST_PATH:
-            dist2min_coin += 2
-
-        # fill features
-        """
-        other posible features
-        f.append(diffmin_xy[0])
-        f.append(diffmin_xy[1])
-        f.append(np.count_nonzero(patch_coinagent))
-        f.append(patch_coinagent.shape[0]* patch_coinagent.shape[1] - np.count_nonzero(patch_coinagent))
-        """
-        f.append(28- dist2min_coin)
-        
-        feature.append(f)
-
-    feature = np.asarray(feature) 
-
-    # because this feature doesn't take in consideration using bombs
-    f_bomb = np.expand_dims(np.zeros(feature.shape[1]), axis=0)
-    feature = np.concatenate((feature,f_bomb), axis=0)
-
-    return feature
+from settings import s
+from random import shuffle
+from agent_code.my_agent.algorithms import *
 
 
-
-
-
-
-##################################################################################################################
+#########################################################################
 
 def setup(self):
     
+    self.actions = [ 'UP', 'DOWN', 'LEFT', 'RIGHT', 'BOMB', 'WAIT' ]
+    self.init_mode = 'initX'
     # load weights
     try:
-        self.weights = np.load('./agent_code/my_agent/models/weights.npy')
+        self.weights = np.load('./agent_code/my_agent/models/NONE.npy')
         print("weights loaded")
     except:
         self.weights = []
@@ -96,120 +22,111 @@ def setup(self):
 
     # Define Rewards
     self.total_R = 0
-
-    # Define possible actions
-    self.actions = ['WAIT','RIGHT', 'LEFT', 'DOWN', 'UP'] #Bomb
     
     # Step size or gradient descent 
     self.alpha = 0.2 
-    self.gamma = 0.9
+    self.gamma = 0.95
+    self.EPSILON = 0.2
+
+#####################################################################
 
 def act(self):
-
+    
     """
-    For the moment only trying to solve the coin detection problem
-    actions and  order: 'WAIT','RIGHT', 'LEFT', 'DOWN', 'UP', 'BOMB'  (SEE: setup)
+    actions order: 'UP', 'DOWN', LEFT', 'RIGHT', 'BOMB', 'WAIT'    
     """
-
+    
+    # load state 
     game_state = self.game_state  # isn't it memory waste calling in each feature extraction for coins, self, arena?
 
-    # Create new feature sates
-    f1 = feat_1(game_state)
-    """
-    Idea would be compute more features:
-    f1 = ... 
-    ...
-    ...
-    f = stack ( all features) 
-    """
-    feature_state = f1
+    # Compute features state 
+    feature_state = feature_extraction(game_state)
     self.prev_state = feature_state
-    print("features computed")
 
-    # later no necessary
-    if self.weights == []:
-        weights = np.ones(feature_state.shape[1])  
-        self.weights = weights
-    else:
-        weights = self.weights
-
-    # TODO:  implement with shuffle for equally best actions ??
-    q_approx = linapprox_q(feature_state, weights)
-    q_next_action = self.actions[np.argmax(q_approx)] #GREEDY POLICY
-
-    self.logger.info('Pick action ')
-#    self.next_action = np.random.choice(['WAIT','RIGHT', 'LEFT', 'DOWN', 'UP'], p=[0.2, .20, .20, .20, .20])
-#    print(game_state['arena'])
-
-    self.next_action = q_next_action
-    print("action", q_next_action)
-     
-        
+    #different initial guesses can be defined here: 
+    if len(self.weights) == 0:
+        print('no weights, init weights')
+        if self.init_mode == 'initX':
+            self.weights = np.array([1,1,-7,-1,4,-0.5,1.5,2,0.5,0.5,-7,1.5,3,2,-1])  
+        elif self.init_mode == 'init1':
+            self.weights = np.ones(feature_state.shape[1])  
+        elif self.init_mode == 'initRand':
+            self.weights = np.random.rand(self.prev_state.shape[1])
+    
     print(self.weights)
 
+    self.logger.info('Pick action')
+    
+    #'''
+    
+    # Linear approximation approach
+    q_approx = np.dot(feature_state, self.weights)    
+    best_actions = np.where(q_approx == np.max(q_approx))[0] 
+    shuffle(best_actions)
+
+    q_next_action = self.actions[best_actions[0]] #GREEDY POLICY
+    self.next_action = q_next_action
+    print("q action picked  ", q_next_action)
+    #'''
+    
+    ####### EPSILON GREEDY (TRAINING) #########################
+    '''
+    greedy = np.random.choice([0,1], p=[self.EPSILON, 1-self.EPSILON])
+    if greedy:
+    
+        q_approx = np.dot(feature_state, self.weights)
+        best_actions = np.where(q_approx == np.max(q_approx))[0] 
+        shuffle(best_actions)
+        
+        q_next_action = s.actions[best_actions[0]] #GREEDY POLICY
+        self.next_action = q_next_action
+        print("q action picked  ", q_next_action)
+
+    else:
+        self.next_action = np.random.choice(['RIGHT', 'LEFT', 'UP', 'DOWN', 'BOMB'], p=[.23, .23, .23, .23, .08])
+        print("random action picked ", self.next_action)
+    '''
+#####################################################################
 def reward_update(self):
 
-    '''
-        IMPORTANT: 
-            reward_update happens AFTER act
-            This means, self.next_action in reward_update is the
-            action the algorithm just took in action. This is why
-            we should rename self.next_action as prev_action in 
-            reward_update
-        
-    '''
     self.logger.info('IN TRAINING MODE ')
+    print('LEARNING')
 
-    reward = 0 
 
-    if self.game_state['step'] != 1:
-        for event in self.events:
-            if event == e.INVALID_ACTION:
-                reward -= 10 
-            elif event == e.COIN_COLLECTED:
-                reward += 100
-            elif event == e.WAITED:
-                reward -= 10 
-            else:
-                reward -= 1
-        
-        self.total_R += reward
-       
-        # load weights
-        weights = self.weights
+    reward = new_reward(self.events)
+    self.total_R += reward        
 
-        # learning 
+    feature_state = feature_extraction(self.game_state)
+    next_state = feature_state
 
-        # Berechene alle features
-        next_state = feat_1(self.game_state)
-        """
-        so wie vorhin und mit stack, vlt, machen wir eine Function die alles auf einmal stack
-        """
-        prev_state = self.prev_state
-        prev_state_a = prev_state[self.actions.index(self.next_action),:]
+    if self.game_state['step'] > 1:
 
-        alpha = self.alpha
-        gamma = self.gamma
+        prev_state_a = self.prev_state[self.actions.index(self.next_action),:]
+
         # update weights
-        weights = q_gd_linapprox(next_state, prev_state_a, reward, weights, alpha, gamma)        
-
-        self.weights = weights
-        self.alpha = 1/self.game_state['step']
-        self.gamma = self.gamma ** self.game_state['step']
+        weights = q_gd_linapprox(next_state, prev_state_a, reward, self.weights, self.alpha, self.gamma)      
+        self.weights = weights        
         
-
+        # update alpha and gamma for convergence
+        self.alpha *= 1/self.game_state['step']
+        #self.gamma = self.gamma ** self.game_state['step']
+        
+#####################################################################
 def end_of_episode(self):
-    np.save('./agent_code/my_agent/models/weights.npy', self.weights)
+
+    ## calculate new weights for last step
+    reward = new_reward(self.events)
+    self.total_R += reward        
     
+    feature_state = feature_extraction(self.game_state)
+    next_state = feature_state
 
-def linapprox_q(state, weights):
-    q_approx = np.dot(state, weights)
-    return q_approx
+    prev_state_a = self.prev_state[self.actions.index(self.next_action),:]
 
-def q_gd_linapprox(next_state, prev_state_a, reward, weights, alpha, gamma):
-    next_state_a = next_state[np.argmax(linapprox_q(next_state, weights)), :]
-    weights += alpha * (reward + gamma * np.dot(next_state_a,weights) - np.dot(prev_state_a,weights)) * prev_state_a 
-    return weights
-            
-#def eps_greedy(self, epsilon = ):
-#    return EPSILON/(C*self.round/s.n_rounds+1)
+    # update weights
+    weights = q_gd_linapprox(next_state, prev_state_a, reward, self.weights, self.alpha, self.gamma)      
+    self.weights = weights 
+
+    ############## SAVING LEARNING FROM ONE EPISODE 
+    np.save('./agent_code/my_agent/models/NONE.npy', self.weights)
+
