cde2b3f30aafb141e07f6382357e988f11e2f9e2
Ferdinand Vanmaele <fvanmaele@ciaconna.org>
2019-03-23 05:26:30 +0100

    fix method calls

--- agent_code/my_agent/learning_methods.py
+++ agent_code/my_agent/learning_methods.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 """
 Learning schedules (compute time steps for iterative methods)
 """
@@ -12,7 +11,7 @@ def learning_schedule_1(time_step, max_steps=400):
     elif (max_steps/2 <= time_step <= max_steps/4):
         return 0.01
 
-def learning_schedule_2(time_step, c=1.0, eta=0.5):
+def learning_schedule_2(time_step, c=1.0, eta=1.0):
     return c / pow(time_step, eta)
 
 def learning_schedule_3(time_step):
@@ -30,9 +29,9 @@ def UpdateWeightsLFA(X, A, R, Y, weights, alpha=0.1, discount=0.95):
     The features X and Y are assumed to have state_action(action) and
     max_q(weights) methods available. See feature_extraction.py for details.
     """
-    X_A = X.state_action[A]
-    Q_max, A_max = Y.max_q(weights)
-    TD_error = R + (discount * Q_max) - np.dot(weights, X_A)
+    X_A = X.state_action(A)
+    Q_max, _ = Y.max_q(weights)
+    TD_error = R + (discount * Q_max) - np.dot(X_A, weights)
 
     return weights + (alpha * TD_error * X_A)
 
@@ -42,16 +41,18 @@ def UpdateWeightsLFA_DoubleQ(X, A, R, Y, weights1, weights2, alpha=0.1, discount
     Update the weight vectors w1, w2 for Double Q-learning.
     """
     Q_choice = np.random_choice(['tails', 'heads'])
-    X_A = X.state_action[A]
+    X_A = X.state_action(A)
 
     if Q_choice == 'heads':
         # update weights1 on heads, use weights2 for approximation
+        # TODO: max_q returns Q_max (real) and A_max (vector)
         Q2 = np.dot(weights2, Y.max_q(weights1))
         TD_error = R + (discount * Q2) - np.dot(weights1, X_A)
 
         return weights1 + (alpha * TD_error * X_A), weights2
     else:
         # update weights2 on tails, use weights1 for approximation
+        # TODO: max_q returns Q_max (real) and A_max (vector)
         Q1 = np.dot(weights1, Y.max_q(weights2))
         TD_error = R + (discount * Q1) - np.dot(weights2, X_A)
 
@@ -65,8 +66,8 @@ def UpdateWeightsLFA_SARSA(X, A, R, Y, B, weights, traces, Lambda=1,
     The features X and Y are assumed to have state_action(action) and
     max_q(weights) methods available. See feature_extraction.py for details.
     """
-    X_A = X.state_action[A]
-    Y_B = Y.state_action[B]    
+    X_A = X.state_action(A)
+    Y_B = Y.state_action(B)
 
     TD_error = R + discount * np.dot(weights, Y_B) - np.dot(weights, X_A)
     traces_n = X_A + discount * Lambda * eligibility_traces
