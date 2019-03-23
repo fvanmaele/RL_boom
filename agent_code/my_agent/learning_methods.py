5cb41b9b338030bfb530643eb5b1fb198e172548
Ferdinand Vanmaele <fvanmaele@ciaconna.org>
2019-03-23 15:56:11 +0100

    learning_methods.py: fix dot product

--- agent_code/my_agent/learning_methods.py
+++ agent_code/my_agent/learning_methods.py
@@ -11,14 +11,14 @@ def learning_schedule_1(time_step, max_steps=400):
     elif (max_steps/2 <= time_step <= max_steps/4):
         return 0.01
 
-def learning_schedule_2(time_step, c=0.1, eta=1.0):
+def learning_schedule_2(time_step, c=0.1, eta=0.5):
     return c / pow(time_step, eta)
 
 def learning_schedule_3(time_step):
     return 10.0 / (9.0 + time_step)
 
-def learning_schedule_4():
-    return 0.1
+def learning_schedule_4(t=0.1):
+    return t
 
 """
 Learning methods (Q-Learning, SARSA)
@@ -47,14 +47,14 @@ def UpdateWeightsLFA_DoubleQ(X, A, R, Y, weights1, weights2, alpha=0.1, discount
         # update weights1 on heads, use weights2 for approximation
         # TODO: max_q returns Q_max (real) and A_max (vector)
         Q2 = np.dot(weights2, Y.max_q(weights1))
-        TD_error = R + (discount * Q2) - np.dot(weights1, X_A)
+        TD_error = R + (discount * Q2) - np.dot(X_A, weights1)
 
         return weights1 + (alpha * TD_error * X_A), weights2
     else:
         # update weights2 on tails, use weights1 for approximation
         # TODO: max_q returns Q_max (real) and A_max (vector)
         Q1 = np.dot(weights1, Y.max_q(weights2))
-        TD_error = R + (discount * Q1) - np.dot(weights2, X_A)
+        TD_error = R + (discount * Q1) - np.dot(X_A, weights2)
 
         return weights1, weights2 + (alpha * TD_error * X_A)
 
@@ -68,7 +68,7 @@ def UpdateWeightsLFA_SARSA(X, A, R, Y, B, weights, z, Lambda=0.5, alpha=0.1, dis
     X_A = X.state_action(A)
     Y_B = Y.state_action(B)
 
-    TD_error = R + discount * np.dot(weights, Y_B) - np.dot(weights, X_A)
+    TD_error = R + discount * np.dot(Y_B, weights) - np.dot(X_A, weights)
     z_next = X_A + discount * Lambda * z
 
     return weights + alpha * TD_error * z_next, z_next
